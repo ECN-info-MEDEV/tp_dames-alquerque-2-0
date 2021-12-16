b07aeb8411eeffc45d33101e67772aca48a8f719
Nartrack <71267249+Nartrack@users.noreply.github.com>
2021-12-16 11:51:45 +0100

    Classe Square

--- app/src/main/java/alquerque/Square.java
+++ app/src/main/java/alquerque/Square.java
@@ -1,5 +1,25 @@
 package alquerque;
 
-public class Square {
+import lombok.Getter;
+import lombok.Setter;
 
+public class Square {
+    @Getter
+    private boolean isPlayable;
+    @Getter
+    @Setter
+    private boolean isOccupied;
+    @Getter
+    @Setter
+    private boolean isWhite;
+    @Getter
+    @Setter
+    private boolean isQueen;
+
+    public Square(boolean isPlayable,boolean isOccupied,boolean isWhite) {
+        this.isPlayable = isPlayable;
+        this.isOccupied = isOccupied;
+        this.isWhite = isWhite;
+        this.isQueen = false;
+    }
 }
