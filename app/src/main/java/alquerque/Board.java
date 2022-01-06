aeab5abf496276db6eec8bf9057a18fbd125b642
rami3l <rami3l@outlook.com>
2022-01-06 09:37:55 +0100

    feat: implement `Square::isPossibleUnmoved`

--- app/src/main/java/alquerque/Board.java
+++ app/src/main/java/alquerque/Board.java
@@ -2,7 +2,6 @@ package alquerque;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import alquerque.utils.Displayable;
 import lombok.Getter;
 import lombok.var;
@@ -63,18 +62,54 @@ public class Board {
         }
     }
 
+    /** Indicates if a movement is possible as a first move. */
     public boolean isPossibleUnmoved(Move movement) {
-        return true;
+        var start = movement.start();
+        var end = movement.end();
+        var x = start.x();
+        var y = start.y();
+        // "forward" is in the -x direction for white, and the +x direction for black.
+        var isWhite = currentPlayer.isWhite();
+        var dx = isWhite ? -1 : 1;
+
+        // forward-right and forward-left squares.
+        var fr = new Position(x + dx, y + 1);
+        var fl = new Position(x + dx, y - 1);
+
+        // backward-right and backward-left squares.
+        var br = new Position(x - dx, y + 1);
+        var bl = new Position(x - dx, y - 1);
+
+        // Indirect diagonal squares.
+        var fr2 = new Position(x + 2 * dx, y + 2);
+        var fl2 = new Position(x + 2 * dx, y - 2);
+        var br2 = new Position(x - 2 * dx, y + 2);
+        var bl2 = new Position(x - 2 * dx, y - 2);
+
+        return
+        // Direct move: if we are moving to a forward diagonal which is not occupied
+        end.equals(fr) && !squareAt(fr).isOccupied() || end.equals(fl) && !squareAt(fl).isOccupied()
+        // Jump: if we are moving to a jump which is occupied, and that the square we are jumping
+        // has an enemy's piece.
+                || end.equals(fr2) && squareAt(fr).isOccupied() && squareAt(fr).isWhite() != isWhite
+                        && !squareAt(fr2).isOccupied()
+                || end.equals(fl2) && squareAt(fl).isOccupied() && squareAt(fl).isWhite() != isWhite
+                        && !squareAt(fl2).isOccupied()
+                || end.equals(br2) && squareAt(br).isOccupied() && squareAt(br).isWhite() != isWhite
+                        && !squareAt(br2).isOccupied()
+                || end.equals(bl2) && squareAt(bl).isOccupied() && squareAt(bl).isWhite() != isWhite
+                        && !squareAt(bl2).isOccupied();
     }
 
     public List<Move> isPossibleMoved(Position start) {
         List<Move> possiblePos = new ArrayList<>();
         List<Position> positions = List.of(new Position(start.x() - 1, start.y() - 1),
-                new Position(start.x() - 1, start.y() + 1), new Position(start.x() + 1, start.y() - 1),
+                new Position(start.x() - 1, start.y() + 1),
+                new Position(start.x() + 1, start.y() - 1),
                 new Position(start.x() + 1, start.y() + 1));
         for (Position pos : positions) {
             Square theCase = this.squareAt(pos);
-            if (theCase.isOccupied() && this.currentPlayer.isWhite() ? !theCase.isWhite() : theCase.isWhite()) {
+            if (theCase.isOccupied() && this.currentPlayer.isWhite() != theCase.isWhite()) {
                 possiblePos.add(new Move(start, pos));
             }
         }
